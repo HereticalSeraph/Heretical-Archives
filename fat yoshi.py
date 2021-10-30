c44938e8f27a5681878a57156d450f3d81cdcd2a
HereticalSeraph <93415424+HereticalSeraph@users.noreply.github.com>
2021-10-30 12:52:12 -0500

    Update fat yoshi.py

--- fat yoshi.py
+++ fat yoshi.py
@@ -1,18 +1,19 @@
-    #importing the necessary modules
+#import modules
 import requests
 import shutil
 import ctypes
+import os
 
-    #grabbing the image from the url
+#grab image from URL
 image_url = "https://www.clipartkey.com/mpngs/m/122-1222244_fat-yoshi-transparent-fat-yoshi-transparent-background.png"
 
-    #store the original file name as a variable for later use
-filename = image_url.split("/")[-1]
+#set file name
+filename = 'temp.png'
 
-    #open the image, return stream contents
+#open image from the URL, return stream contents
 r = requests.get(image_url, stream = True)
 
-    #check if the image was correctly retrieved from the URL
+#check if image was successfully retrieved from the URL
 if r.status_code == 200:
     r.raw.decode_content = True
     with open(filename, 'wb') as f:
@@ -20,5 +21,8 @@ if r.status_code == 200:
 else:
     print('whoops')
 
-    #change the wallpaper to the downloaded image
-ctypes.windll.user32.SystemParametersInfoW(20,0,filename,0)
+#find globl path to temporary file
+global_path = os.path.join(os.path.dirname(__file__), filename)
+
+#set wallpaper to downloaded image
+ctypes.windll.user32.SystemParametersInfoW(20,0,global_path,0)
